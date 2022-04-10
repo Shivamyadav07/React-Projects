ea3807feed254f4fb4599091713dfe8ab92a9da0
Shivamyadav07 <shivamvnsindia@gmail.com>
2022-04-10 18:41:03 +0530

    2

--- Webpack ToDo/src/index.js
+++ Webpack ToDo/src/index.js
@@ -1,4 +1,15 @@
 
 const root=document.querySelector("#root")
 
-document.createElement("")
+const inputfield=document.createElement("textarea");
+const btn =document.createElement("input");
+btn.setAttribute("type","button");
+btn.setAttribute("class","mybutton");
+btn.addEventListener("click",myfunction)
+
+root.append(inputfield,btn);
+function myfunction()
+{
+    const data=inputfield.value;
+    console.log(data);
+}
