f49b5fd6e7fdb0780181db6c20b97be3b0d537da
Shivamyadav07 <shivamvnsindia@gmail.com>
2022-04-13 19:58:18 +0530

    update app.js

--- Day2 React Intro/react-navbar/src/App.js
+++ Day2 React Intro/react-navbar/src/App.js
@@ -1,12 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import image from "./logo bakery.png";
+import "./App.css";
 
 function App() {
+  const links = ["Services", "Projects", "About"];
   return (
     <div className="App">
-      ``
+      <div className="innerdiv">
+        <img src={image} alt="shivam"></img>
+        {links.map((e) => {
+          return <Link className="App" shivam={e} />;
+        })}
+        <button>Contact</button>
+      </div>
     </div>
   );
 }
 
+function Link({ shivam }) {
+  return <p className="links">{shivam} </p>;
+}
 export default App;
